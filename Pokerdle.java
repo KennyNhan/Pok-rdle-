d121e24501bd3c1c60d31278614cdc3eab80df1a
KennyNhan <kendoro123@gmail.com>
2022-04-22 13:17:26 -0500

    Get random pokemon

--- Pokerdle.java
+++ Pokerdle.java
@@ -12,14 +12,19 @@ public class Pokerdle {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(new File("pokemon.txt")));
-        Map<String, String> integerDateMap = new HashMap<>(); // Map to put data
+        Map<String, String> pokemonMap = new HashMap<>(); // Map to put data
+        ArrayList<String> pokemon = new ArrayList<>();
         String line = null;
         line = bufferedReader.readLine(); // neglect first line
         while((line = bufferedReader.readLine())!= null){
              String[] split = line.split(":");
-             integerDateMap.put(split[0], split[1]);
+             pokemon.add(split[0]);
+             pokemonMap.put(split[0], split[1]);
         }
-
+        Random r = new Random();
+        int upperBound = pokemon.size();
+        String randomPokemon = pokemon.get(r.nextInt(upperBound));
+        
     }
 
 
